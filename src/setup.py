1ce53210e7ad7ae7296762b219ceb1cb9b8301db
Pablo Klijnjan <pabloklijnjan@gmail.com>
2011-12-04 17:55:07 +0200

    Trivial: Added setup.py

---
+++ src/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+import dmtcore
+
+METADATA = dict(
+    name='dmtcore',
+    version=dmtcore.__version__,
+    author='Pablo Klijnan',
+    author_email='pabloklijnjan@gmail.com',
+    description='',
+    long_description='',
+    url='http://github.com/holandes22/dmtcore',
+    include_package_data=True,
+    classifiers=[
+        'Development Status :: 0 - Alpha',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ],
+    zip_safe=False,
+    packages=find_packages(),
+    package_data={
+        'dmtcore': []
+    }
+)
+
+if __name__ == '__main__':
+    setup(**METADATA)
