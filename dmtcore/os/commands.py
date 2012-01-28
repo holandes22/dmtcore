903f3612a595f8430fd87a9d6b4fa8d1ab03921c
Pablo Klijnjan <pabloklijnjan@gmail.com>
2012-01-28 19:29:06 +0200

    9 - Adding logger to os.commands module

--- dmtcore/os/commands.py
+++ dmtcore/os/commands.py
@@ -1,12 +1,16 @@
+import logging
 from subprocess import check_output, STDOUT, CalledProcessError
 
-#List of approved commands
+module_logger = logging.getLogger("dmtcore.os.commands")
 
+#List of approved commands
 SIZE_FROM_FDISK = ['/sbin/fdisk', '-l']
 
 APPROVED_CMDS = [
                  SIZE_FROM_FDISK,
                  ]
+
+module_logger.info("List of Approved commands: %s" % APPROVED_CMDS)
 
 def run_cmd(cmd, run_as_sudo = True):
     #For dynamic cmds, usually the last arg is the one added dinamically
@@ -16,11 +20,14 @@ def run_cmd(cmd, run_as_sudo = True):
             if run_as_sudo:
                 cmd.insert(0, "sudo")
             #TODO: log INFO running cmd blah
+            module_logger.info("Running command {0}".format(cmd))
             return check_output(cmd, stderr = STDOUT)
-        except CalledProcessError:
+        except CalledProcessError as e:
             #TODO: log ERROR error and error.returncode
+            module_logger.error("Error caught while running command {0}. {1}".format(cmd, e))
             raise
     else:
         #TODO: Raise a more meaningful error
+        module_logger.error("Running command {0} is not allowed. Verify it belongs to the approved commands list.".format(cmd))
         raise AttributeError
 
