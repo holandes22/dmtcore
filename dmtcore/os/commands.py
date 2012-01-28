686d09a4782223c64a9be0f96458bf20155d8517
Pablo Klijnjan <pabloklijnjan@gmail.com>
2012-01-28 10:47:25 +0200

    6 - Dealing with dynamic commands
        - In a very simple manner for now. Only discarding the
          last arg passed as it is usually the dynamic one.

--- dmtcore/os/commands.py
+++ dmtcore/os/commands.py
@@ -1,17 +1,23 @@
 from subprocess import check_output, STDOUT, CalledProcessError
 
+#List of approved commands
+
+from dmtcore.os.disk.linux import SIZE_FROM_FDISK
+
 SUDO = "sudo"
-FDISK_LIST = ["fdisk", "-l"]
 
 APPROVED_CMDS = [
-   FDISK_LIST, 
+   SIZE_FROM_FDISK
 ]
 
 for cmd in APPROVED_CMDS:
     cmd.insert(0, SUDO)
 
 def run_cmd(cmd):
-    if cmd not in APPROVED_CMDS:
+    #For dynamic cmds, usually the last arg is the one added dinamically
+    #TODO: Find a better wat to deal with this: issue 6
+    if cmd not in APPROVED_CMDS or cmd[:-1] not in APPROVED_CMDS:
+        #TODO: Raise a more meaningful error
         raise AttributeError
     try:
         #TODO: log INFO running cmd blah
