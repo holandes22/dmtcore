1b65d447ff502dc43e723db48fc7272dcecc7977
Pablo Klijnjan <pabloklijnjan@il.ibm.com>
2012-03-16 19:33:37 +0200

    8: Obtaining hctl from /sys/block/<name>/device link
        - Is much simple that mapping hctl's from /proc/scsi/scsi and
          mapping to path in /sys/block/<name>/scsi_disk:h:c:t:l
        - Works in accross all distros

--- dmtcore/os/disk/linux.py
+++ dmtcore/os/disk/linux.py
@@ -17,22 +17,22 @@ class LinuxDeviceMapper(object):
     pass
 
 class LinuxDiskDeviceQueries(DiskDeviceQueries):
-    
+
     def _populate_disks_entries(self):
         device_filepaths = glob('/dev/sd*[!0-9]')
         device_names = [os.path.basename(device_filepath) for device_filepath in device_filepaths]
         self.hctl_map = self._map_hctl_to_disk_device_names(device_names)
-        
+
         for device_filepath, device_name in map(None, device_filepaths, device_names):
             size = self._extract_size_from_fdisk(device_filepath)
             major_minor = get_major_minor(device_filepath)
             self.basic_disk_entries.append(DiskEntry(device_name, device_filepath, size, major_minor))
-    
+
         self.multipath_disk_entries = []
-    
+
     def get_partition_entries(self, device_name):
         return self._get_sysfs_partitions(device_name)
-        
+
     def _get_sysfs_partitions(self, device_name):
         partitions = []
         partition_filepaths = glob('/dev/%s[0-9]*' % device_name)
@@ -40,14 +40,14 @@ class LinuxDiskDeviceQueries(DiskDeviceQueries):
             size = self._extract_size_from_fdisk(partition_filepath)
             major_minor = get_major_minor(partition_filepath)
             partitions.append(DiskEntry(
-                                        os.path.basename(partition_filepath), 
-                                        partition_filepath, 
-                                        size, 
+                                        os.path.basename(partition_filepath),
+                                        partition_filepath,
+                                        size,
                                         major_minor,
                                         )
                               )
         return partitions
-    
+
     def _device_name_is_partition(self, device_name):
         """
         Receives a device name and indicates if it is a partition or not:
@@ -56,7 +56,7 @@ class LinuxDiskDeviceQueries(DiskDeviceQueries):
         sda1b, /dev/sda1 ->False, not valid input
         """
         return  re.compile("^\w+\d+$").match(device_name) is not None
-    
+
     def _extract_size_from_fdisk(self, device_filepath):
         """/sys/block/sda/device/block/sda
         :returns: The size in bytes of the specified device
@@ -72,31 +72,30 @@ class LinuxDiskDeviceQueries(DiskDeviceQueries):
                 except ValueError:
                     module_logger.warning("Cannot get size for device {0}.\
                                                             Output: {1}".format(device_filepath, size))
-                    return None    
+                    return None
         return None
-    
+
     def get_hctl(self, device_name):
         try:
             return self.hctl_map[device_name]
         except KeyError:
             module_logger.warning("Cannot get hctl for device {0}".format(device_name))
             return None
-    
-    def _get_all_hctls(self):
-        if getattr(self, '_all_hctls', None) is None:
-            self._all_hctls =  self._extract_all_hctls_from_proc_scsi_file()
-        return self._all_hctls 
-  
+
     def _map_hctl_to_disk_device_names(self, device_names):
         hctl_map = {}
         for device_name in device_names:
-            for hctl in self._get_all_hctls():
-                d =  {'name': device_name,'h': hctl.host, 'c': hctl.channel, 't': hctl.scsi_id, 'l': hctl.lun_id}
-                path = "/sys/block/{name}/device/scsi_disk:{h}:{c}:{t}:{l}".format(**d)
-                if os.path.exists(path):
-                    hctl_map[device_name] = hctl
+            hctl_map[device_name] = self._extract_hctl_from_device_link(device_name)
         return hctl_map
-    
+
+    def _extract_hctl_from_device_link(self, device_name):
+        path = "/sys/block/{0}/device".format(device_name)
+        try:
+            h,c,t,l = os.readlink(path).split("/")[-1].split(":")
+        except OSError:
+            return None
+        return HctlInfo(int(h),int(c),int(t),int(l))
+
     def _extract_all_hctls_from_proc_scsi_file(self):
         hctls = []
         hctl_line_re= re.compile("""
@@ -106,7 +105,7 @@ class LinuxDiskDeviceQueries(DiskDeviceQueries):
                                     Lun:\s+(?P<lun_id>\d+)\s*
                                 """, re.VERBOSE)
 
-        
+
         with open("/proc/scsi/scsi", "r") as f:
             for line in f.readlines():
                 m = hctl_line_re.match(line)
@@ -120,4 +119,4 @@ class LinuxDiskDeviceQueries(DiskDeviceQueries):
                                           )
                                  )
         return hctls
-    
+
--- tests/dmtcore/os/disk/test_linux.py
+++ tests/dmtcore/os/disk/test_linux.py
@@ -32,7 +32,7 @@ FAKE_FDISK_BAD_OUTPUT = ""
 
 FAKE_CONTENT_OF_PROC_SCSI = """
 Attached devices:
-Host: scsi0 Channel: 00 Id: 00 Lun: 00 
+Host: scsi0 Channel: 00 Id: 00 Lun: 00
   Vendor: TSSTcorp Model: CDDVDW TS-L632H  Rev: TO01
   Type: CD-ROM ANSI  SCSI revision: 05
 Host: scsi1 Channel: 00 Id: 02 Lun: 00
@@ -48,33 +48,35 @@ Host: scsi1 Channel: 00 Id: 03 Lun: 01
   Vendor: IBM Model: 2107900 Rev: 5.53
   Type: Direct-Access ANSI SCSI revision: 05
 Host: scsi0 Channel: 00 Id: 00 Lun: 00
-  Vendor: ATA Model: VBOX HARDDISK Rev: 1.0 
+  Vendor: ATA Model: VBOX HARDDISK Rev: 1.0
   Type: Direct-Access ANSI SCSI revision: 05
 """
 
-def fake_path_exists(path):
-    valid_paths = [
-                   "/sys/block/sda/device/scsi_disk:0:0:0:0",
-                   "/sys/block/sdb/device/scsi_disk:1:0:2:0",
-                   "/sys/block/sdc/device/scsi_disk:1:0:2:1",
-                   ]
-    return path in valid_paths
+
+def readlink_side_effect(device_name):
+    links = {
+             "/sys/block/sda/device": "../../../0:0:0:0",
+             "/sys/block/sdb/device": "0000:00/0000:00:10:0/host0/target0:0:0/0:0:0:0",
+             "/sys/block/sdc/device": "platform/host1/session1/target1:0:0:1/1:0:0:3",
+             "/sys/block/sdd/device": "../../../15:16:17:333",
+             }
+    return links[device_name]
 
 class TestLinuxDiskDeviceQueries(unittest.TestCase):
-    
+
     @patch.object(LinuxDiskDeviceQueries, "_populate_disks_entries")
     def test__device_name_is_partition__device_is_a_partition(self, _populate_disks_entries_mock):
         dq = LinuxDiskDeviceQueries()
         self.assertEqual(1, _populate_disks_entries_mock.call_count)
         self.assertFalse(dq._device_name_is_partition("sda"))
         self.assertFalse(dq._device_name_is_partition("sdab"))
-    
+
     @patch.object(LinuxDiskDeviceQueries, "_populate_disks_entries")
     def test__device_name_is_partition__device_is_not_a_partition(self, _populate_disks_entries_mock):
         dq = LinuxDiskDeviceQueries()
         self.assertTrue(dq._device_name_is_partition("sda1"))
         self.assertTrue(dq._device_name_is_partition("sdab12"))
-    
+
     @patch.object(LinuxDiskDeviceQueries, "_populate_disks_entries")
     def test__device_name_is_partition__device_is_not_valid(self, _populate_disks_entries_mock):
         dq = LinuxDiskDeviceQueries()
@@ -90,7 +92,7 @@ class TestLinuxDiskDeviceQueries(unittest.TestCase):
         actual_size = dq._extract_size_from_fdisk(fake_device_filepath)
         run_cmd_mock.assert_called_once_with(SIZE_FROM_FDISK + [fake_device_filepath])
         self.assertEqual(8185184256, actual_size)
-        
+
     @patch.object(linux, "run_cmd")
     @patch.object(LinuxDiskDeviceQueries, "_populate_disks_entries")
     def test__extract_size_from_fdisk_good_output_partition(self, _populate_disks_entries_mock, run_cmd_mock):
@@ -107,34 +109,16 @@ class TestLinuxDiskDeviceQueries(unittest.TestCase):
         run_cmd_mock.return_value = FAKE_FDISK_BAD_OUTPUT
         dq = LinuxDiskDeviceQueries()
         self.assertEqual(None, dq._extract_size_from_fdisk("/dev/sda"))
-    
-    @patch('os.path.exists', new = fake_path_exists)
-    @patch.object(LinuxDiskDeviceQueries, "_get_all_hctls")
-    @patch.object(LinuxDiskDeviceQueries, "_populate_disks_entries")    
-    def test__map_hctl_to_disk_device_name(self, _populate_disks_entries_mock, get_all_hctls_mock):
-        get_all_hctls_mock.return_value = [
-                                           HctlInfo(0,0,0,0), 
-                                           HctlInfo(1,0,2,0), 
-                                           HctlInfo(1,0,2,1), 
-                                           HctlInfo(0,0,0,0),
-                                           ]
-        expected_hctl_map = {
-                           'sda':HctlInfo(0,0,0,0),
-                           'sdb':HctlInfo(1,0,2,0),
-                           'sdc':HctlInfo(1,0,2,1),
-                           }
-        dq = LinuxDiskDeviceQueries()
-        self.assertEqual(expected_hctl_map, dq._map_hctl_to_disk_device_names(expected_hctl_map.keys()))
-    
-    @patch.object(LinuxDiskDeviceQueries, "_populate_disks_entries")    
+
+    @patch.object(LinuxDiskDeviceQueries, "_populate_disks_entries")
     def test__extract_all_hctls_from_proc_scsi_file(self, _populate_disks_entries_mock):
-        
+
         with patch('dmtcore.os.disk.linux.open', create = True) as open_mock:
             open_mock.return_value = MagicMock(spec = file)
 
             file_handle = open_mock.return_value.__enter__.return_value
             file_handle.readlines.return_value = FAKE_CONTENT_OF_PROC_SCSI.splitlines()
-            
+
             dq = LinuxDiskDeviceQueries()
             expected_results = [
                                HctlInfo(0,0,0,0),
@@ -146,12 +130,12 @@ class TestLinuxDiskDeviceQueries(unittest.TestCase):
                                ]
             actual_results = dq._extract_all_hctls_from_proc_scsi_file()
             self.assertEqual(len(expected_results), len(actual_results))
-            
+
             for expected_result, actual_result in map(None, expected_results, actual_results):
                 self.assertEqual(expected_result, actual_result)
-    
+
     @patch.object(linux, 'get_major_minor', new = lambda x: (250, int(x[-1])))
-    @patch.object(linux, 'glob')            
+    @patch.object(linux, 'glob')
     @patch.object(LinuxDiskDeviceQueries, "_extract_size_from_fdisk")
     @patch.object(LinuxDiskDeviceQueries, "_populate_disks_entries")
     def test__get_sysfs_partitions(self, _populate_disks_entries_mock, extract_size_mock, glob_mock):
@@ -163,7 +147,7 @@ class TestLinuxDiskDeviceQueries(unittest.TestCase):
                             DiskEntry('sda1', '/dev/sda1', fake_size, (250, 1)),
                             DiskEntry('sda2', '/dev/sda2', fake_size, (250, 2)),
                             DiskEntry('sda3', '/dev/sda3', fake_size, (250, 3)),
-                            ] 
+                            ]
         actual_results = dq._get_sysfs_partitions('sda')
         self.assertEqual(len(expected_results), len(actual_results))
         for expected_result, actual_result in map(None, expected_results, actual_results):
@@ -171,4 +155,37 @@ class TestLinuxDiskDeviceQueries(unittest.TestCase):
             self.assertEqual(expected_result.filepath, actual_result.filepath)
             self.assertEqual(expected_result.size, actual_result.size)
             self.assertEqual(expected_result.major_minor, actual_result.major_minor)
+
+    @patch('os.readlink')
+    @patch.object(LinuxDiskDeviceQueries, "_populate_disks_entries")
+    def test__extract_hctl_from_device_link(self, _populate_disks_entries_mock, readlink_mock):
+        device_names = [
+                 "sda",
+                 "sdb",
+                 "sdc",
+                 "sdd",
+                 ]
+        expected_results = [
+                            HctlInfo(0,0,0,0),
+                            HctlInfo(0,0,0,0),
+                            HctlInfo(1,0,0,3),
+                            HctlInfo(15,16,17,333),
+                            ]
+
+        readlink_mock.side_effect = readlink_side_effect
+        dq = LinuxDiskDeviceQueries()
+        for expected_result, device_name in map(None, expected_results, device_names):
+            self.assertEqual(expected_result, dq._extract_hctl_from_device_link(device_name))
+
+    @patch('os.readlink')
+    @patch.object(LinuxDiskDeviceQueries, "_populate_disks_entries")
+    def test__extract_hctl_from_device_link_returns_none_on_oserror(
+                                                                    self, 
+                                                                    _populate_disks_entries_mock, 
+                                                                    readlink_mock
+                                                                    ):
+        readlink_mock.side_effect = OSError()
+        dq = LinuxDiskDeviceQueries()
+        self.assertEqual(None, dq._extract_hctl_from_device_link("sda"))
+        
 
