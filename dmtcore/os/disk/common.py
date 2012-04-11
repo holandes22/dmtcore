6ebce4e8e60009a58089a9da3db96b1ec273cf87
Pablo Klijnjan <pabloklijnjan@il.ibm.com>
2012-04-11 18:03:38 +0300

    Trivial: Being more explicit in the MajorMinor object returned by get_major_minor

--- dmtcore/os/disk/common.py
+++ dmtcore/os/disk/common.py
@@ -3,6 +3,7 @@ from collections import namedtuple
 
 MajorMinor = namedtuple('MajorMinor', 'major,minor')
 
+
 def get_major_minor(device_filepath):
     stat = os.stat(device_filepath)
-    return MajorMinor(os.major(stat.st_rdev), os.minor(stat.st_rdev))
+    return MajorMinor(major = os.major(stat.st_rdev), minor = os.minor(stat.st_rdev))
